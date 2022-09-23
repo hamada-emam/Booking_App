10290a1553154c08803d4626f7b50fb4641b8a4d
beshoyfayz201 <beshoyfayz201@github.com>
2022-09-23 16:23:00 -0700

    fix:diohelper

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:bloc/bloc.dart';
 import 'package:booking_app/src/app/app.dart';
-import 'package:booking_app/src/app/core/helpers/api_helpert.dart';
 import 'package:booking_app/src/app/injector.dart';
 import 'package:booking_app/src/app/core/utils/bloc_observer.dart';
 import 'package:flutter/material.dart';
