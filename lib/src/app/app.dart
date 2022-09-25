3f869ae9abb3c31382b531a1d6e876802923a9c7
emamHamada <hamada152emam@gmail.com>
2022-09-25 17:40:58 +0200

    pull request

--- lib/src/app/app.dart
+++ lib/src/app/app.dart
@@ -37,3 +37,5 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
--- lib/src/features/home/presentation/control/home_cubit/cubit/home_cubit.dart
+++ lib/src/features/home/presentation/control/home_cubit/cubit/home_cubit.dart
@@ -1,5 +1,3 @@
-import 'package:bloc/bloc.dart';
-import 'package:booking_app/src/app/config/routes/routes.dart';
 import 'package:booking_app/src/features/explore_hotels/presentation/screens/explore_screen.dart';
 import 'package:booking_app/src/features/home/presentation/views/profile/description.dart';
 import 'package:flutter/cupertino.dart';
@@ -11,7 +9,7 @@ class HomeCubit extends Cubit<HomeState> {
   HomeCubit() : super(HomeInitial());
   int index = 0;
   List<Widget> screens =
-      List<Widget>.from([(ExploreScreen()), ExploreScreen(), ProfileScreen()]);
+      List<Widget>.from([(ExploreScreen()), ExploreScreen(), const ProfileScreen()]);
   static HomeCubit get(BuildContext context) => BlocProvider.of(context);
   double opacity = 0;
   double margin = 100;
